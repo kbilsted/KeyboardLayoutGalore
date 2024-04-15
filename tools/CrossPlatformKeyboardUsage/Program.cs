ac398a37a75c3849b3dd44853cea2650cd1a5032
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2024-04-16 01:19:57 +0200

    print

--- tools/CrossPlatformKeyboardUsage/Program.cs
+++ tools/CrossPlatformKeyboardUsage/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        using var hook = new TaskPoolGlobalHook();
+        using var hook = new SimpleGlobalHook(GlobalHookType.Keyboard); // new TaskPoolGlobalHook();
         hook.KeyPressed += OnKeyPressed;
         hook.KeyReleased += OnKeyReleased;
         hook.KeyTyped += OnKeyTyped;
@@ -16,19 +16,19 @@ class Program
     private static void OnKeyPressed(object? sender, KeyboardHookEventArgs e)
     {
         var data = e.Data;
-        Console.WriteLine($"Press: char: {data.KeyChar} code: {data.KeyCode} rawchar: {data.RawKeyChar} rawcode: {data.RawCode} all: {data}");
+        Console.WriteLine($"P: char: {data.KeyChar} code: {data.KeyCode} rawchar: {data.RawKeyChar} rawcode: {data.RawCode} all: {data}");
     }
 
     private static void OnKeyTyped(object? sender, KeyboardHookEventArgs e)
     {
         var data = e.Data;
-        Console.WriteLine($"Typed: char: {data.KeyChar} code: {data.KeyCode} rawchar: {data.RawKeyChar} rawcode: {data.RawCode} all: {data}");
+        Console.WriteLine($"T: char: {data.KeyChar} code: {data.KeyCode} rawchar: {data.RawKeyChar} rawcode: {data.RawCode} all: {data}");
     }
 
     private static void OnKeyReleased(object? sender, KeyboardHookEventArgs e)
     {
         var data = e.Data;
-        Console.WriteLine($"Release: char: {data.KeyChar} code: {data.KeyCode} rawchar: {data.RawKeyChar} rawcode: {data.RawCode} all: {data}");
+        Console.WriteLine($"R: char: {data.KeyChar} code: {data.KeyCode} rawchar: {data.RawKeyChar} rawcode: {data.RawCode} all: {data}");
     }
 
 }
